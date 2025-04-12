21aa315b51d55c352651b7e70c45c607c47cc3f4
dtsivkovski <dtsivkovs@gmail.com>
2025-04-11 18:52:22 -0700

    Initial Commit

---
+++ GroceryProject/GroceryProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryProjectApp.swift
+//  GroceryProject
+//
+//  Created by Daniel Tsivkovski on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GroceryProjectTests/GroceryProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryProjectTests.swift
+//  GroceryProjectTests
+//
+//  Created by Daniel Tsivkovski on 4/11/25.
+//
+
+import Testing
+@testable import GroceryProject
+
+struct GroceryProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
